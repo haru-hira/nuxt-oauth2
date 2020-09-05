70bd11bdf1d3b792bde0688ef79c2130f3fe6483
admin_haru <moto55.haru13@gmail.com>
2020-09-05 15:19:28 +0900

    added cookie set/get sample.

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,3 +1,19 @@
 <template>
   <div></div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    // See https://auth.nuxtjs.org/api/storage.html#cookies
+    /* const expiration = this.$auth.$storage.getCookie(
+      '_token_expiration.keycloak'
+    )
+    const expirationDate = new Date(expiration)
+    this.$auth.$storage.setCookie(
+      'expiration_date',
+      expirationDate.toLocaleString({ timeZone: 'Asia/Tokyo' })
+    ) */
+  },
+}
+</script>
