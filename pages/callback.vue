cff3d873a0375acccae0eb99f0ffc6c7fcb2ba07
admin_haru <moto55.haru13@gmail.com>
2020-09-07 23:49:02 +0900

    removed comments.

--- pages/callback.vue
+++ pages/callback.vue
@@ -6,14 +6,14 @@
 export default {
   mounted() {
     // See https://auth.nuxtjs.org/api/storage.html#cookies
-    /* const expiration = this.$auth.$storage.getCookie(
+    const expiration = this.$auth.$storage.getCookie(
       '_token_expiration.keycloak'
     )
     const expirationDate = new Date(expiration)
     this.$auth.$storage.setCookie(
       'expiration_date',
       expirationDate.toLocaleString({ timeZone: 'Asia/Tokyo' })
-    ) */
+    )
   },
 }
 </script>
